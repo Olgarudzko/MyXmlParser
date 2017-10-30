34b2e21c1b937aae6fd1e910385c4e9d45e2aa98
Olgarudzko <leta.rudzko@gmail.com>
2017-10-30 15:53:26 +0300

    Catching all exceptions directly in parsing method

--- src/main/java/by/tc/task02/service/implementaion/XmlParsingService.java
+++ src/main/java/by/tc/task02/service/implementaion/XmlParsingService.java
@@ -17,48 +17,43 @@ public class XmlParsingService implements ParsingService {
 
     private static final String NULL_SOURCE = "Source is null";
     private static final String CHARSET = "UTF-8";
-    private static final String FILE_NOT_FOUND = "File not found";
-    private static final String ERROR_READING_FILE = "Error reading file";
     private static final String SPACES_BETWEEN_TAGS = ">(\\s)+<";
     private static final String TAGS_WITHOUT_SPACES = "><";
 
     public XmlLevel parse(String source) {
-        String xml = readXmlFromFile(getClass(), source);
+
         try {
+            String xml = readXmlFromFile(getClass(), source);
             Validator.isXmlValid(xml);
             DAOFactory factory = DAOFactory.getInstance();
             XmlDao xmlDAO = factory.getXmlDao();
             return xmlDAO.parseXml(xml);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+
         } catch (XmlValidationException e) {
             System.out.println(e.getMessage());
         }
+
         return null;
     }
 
-    private static String readXmlFromFile(Class context, String name) {
+    private static String readXmlFromFile(Class context, String name) throws IOException, XmlValidationException {
 
         String xml = "";
         FileInputStream catalogueReader = null;
+        URL source = context.getClassLoader().getResource(name);
 
-        try {
-            URL source = context.getClassLoader().getResource(name);
-            if (source == null) {
-                System.out.println(NULL_SOURCE);
-                return null;
-            }
-            catalogueReader = new FileInputStream(new File(source.getFile()));
-            byte[] content = new byte[catalogueReader.available()];
-            catalogueReader.read(content);
-            String parsed = new String(content, CHARSET);
-            xml = parsed.replaceAll(SPACES_BETWEEN_TAGS, TAGS_WITHOUT_SPACES);
-
-        } catch (FileNotFoundException e) {
-            System.out.println(FILE_NOT_FOUND);
-            return null;
-        } catch (IOException e) {
-            System.out.println(ERROR_READING_FILE);
-            return null;
+        if (source == null) {
+            throw new XmlValidationException(NULL_SOURCE);
         }
+
+        catalogueReader = new FileInputStream(new File(source.getFile()));
+        byte[] content = new byte[catalogueReader.available()];
+        catalogueReader.read(content);
+        String parsed = new String(content, CHARSET);
+        xml = parsed.replaceAll(SPACES_BETWEEN_TAGS, TAGS_WITHOUT_SPACES);
 
         return xml;
     }
